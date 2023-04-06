b71f0eefd83b5c8c535be66dbdb19906d4b356c7
Charles Swaleh <charlswall@live.com>
2023-04-06 19:21:03 +0300

    Created helper methods for the Vehicle class to get the escort and the senior passenger

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,4 +2,12 @@ class Vehicle < ActiveRecord::Base
   belongs_to :route
   belongs_to :driver
   has_many :passengers
+
+  def senior_passenger
+    self.passengers.select { |passenger| passenger.role == "Senior Passenger" }
+  end
+
+  def escort
+    self.passengers.select { |passenger| passenger.role == "Escort" }
+  end
 end
