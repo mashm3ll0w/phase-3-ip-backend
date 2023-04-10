2d9da596ea8e7045eb117e637ea779c80bce7522
Charles Swaleh <charlswall@live.com>
2023-04-10 11:52:55 +0300

    Created the GET routes for all the classes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,24 @@
 class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
-  
+  set :default_content_type, "application/json"
+
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/routes" do
+    routes = Route.all
+    routes.to_json(include: %i[vehicles drivers passengers])
   end
 
+  get "/vehicles" do
+    vehicles = Vehicle.all
+    vehicles.to_json(include: :driver)
+  end
+
+  get "/drivers" do
+    drivers = Driver.all
+    drivers.to_json(include: %i[vehicles routes])
+  end
+
+  get "/passengers" do
+    pax = Passenger.all
+    pax.to_json
+  end
 end
